1a3a0d1c35ee36e83faf1d43f1d6631f30a3b7d4
TimothySjiang <shjiang@ucdavis.edu>
2019-08-13 06:11:33 -0700

    front end

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Singin extends Component{
 	}
 
 	onSubmitSignIn = () => {	
-		fetch('http://localhost:3000/signin',{
+		fetch(' https://damp-dusk-15044.herokuapp.com/signin',{
 			method:'post',
 			headers:{'Content-Type':"application/json"},
 			body: JSON.stringify({
